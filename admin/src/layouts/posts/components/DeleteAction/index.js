0b1a5526824d778ff85efbac8432c5d0bbc53eda
TADAinGit <nguyenhuynhtatdat@gmail.com>
2022-07-18 12:28:08 +0700

    Update

---
+++ admin/src/layouts/posts/components/DeleteAction/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { DeleteOutline } from "@mui/icons-material";
+import ConfirmDialog from "components/dialog/confirm-dialog";
+import SoftButton from "components/SoftButton";
+import SoftTypography from "components/SoftTypography";
+
+const DeleteAction = ({ params }) => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <SoftButton
+        variant="gradient"
+        style={{ marginLeft: 16 }}
+        color="error"
+        onClick={() => setOpen(true)}
+      >
+        <DeleteOutline />
+      </SoftButton>
+      <ConfirmDialog
+        open={open}
+        title={`Xóa khóa học`}
+        setOpen={setOpen}
+        deleted
+        onAccept={() => {}}
+      >
+        <SoftTypography variant="body2">
+          Bạn có chắc chắn muốn xóa bài viết {params.row.title} này không ?
+        </SoftTypography>
+      </ConfirmDialog>
+    </>
+  );
+};
+
+export default DeleteAction;
--- admin/src/layouts/posts/components/PostTable/index.js
+++ admin/src/layouts/posts/components/PostTable/index.js
@@ -1,58 +1,82 @@
 import PropTypes from "prop-types";
-import { Box, Chip, Paper, Stack } from "@mui/material";
+import { Box, Button, Chip, Paper, Stack } from "@mui/material";
 import RenderTable from "components/RenderTable";
 import React, { useCallback, useEffect, useMemo } from "react";
 import { Link } from "react-router-dom";
-import { Visibility, Edit } from "@mui/icons-material";
+import { Visibility, Edit, Delete, VisibilityRounded } from "@mui/icons-material";
 import { getRandomItem } from "utils/array-utils";
 import colors from "utils/colors";
 import Image from "components/Image";
 import SoftButton from "components/SoftButton";
-import { getAllFeeds } from "../../../../services/api/blogAPI";
+import { getAllFeeds } from "services/api/blogAPI";
+import { getAccountInfor } from "services/api/accountAPI";
+import SoftTypography from "components/SoftTypography";
+import DeleteAction from "../DeleteAction";
+import { format } from "date-fns";
 
-const PostTable = () => {
-  // const dispatch = useDispatch();
+const PostTable = (props) => {
+  const { setPost } = props;
   const columns = [
     {
       headerName: "Tác giả",
       field: "creator",
       width: 200,
-      valueGetter: ({ row }) => row.creator,
+      valueGetter: ({ row }) => {
+        return row.creator;
+      },
     },
     {
       headerName: "Ảnh nền",
       field: "backgroundUrl",
       width: 210,
       renderCell: ({ row }) => (
-        <Image className="aspect-video w-full p-2" src={row.backgroundUrl} />
+        <Image
+          sx={{ aspectRatio: "16/9", p: 1, width: "100%", objectFit: "cover" }}
+          src={row.backgroundUrl}
+        />
       ),
     },
     {
       headerName: "Tiêu đề",
       field: "title",
-      valueGetter: ({ row }) => row.title,
+      valueGetter: ({ row }) => row.title || "(Trống)",
+      flex: 1,
+      minWidth: 80,
     },
     {
       headerName: "Mô tả",
       field: "description",
-      valueGetter: ({ row }) => row.description,
-      editable: true,
+      valueGetter: ({ row }) => row.description || "(Trống)",
+      // editable: true,
+      flex: 1,
+      minWidth: 80,
     },
-    {
-      headerName: "Đường dẫn",
-      field: "url",
-      valueGetter: ({ row }) => row.url,
-      editable: true,
-    },
+    // {
+    //   headerName: "Đường dẫn",
+    //   field: "url",
+    //   valueGetter: ({ row }) => row.url,
+    //   editable: true,
+    // },
     {
       headerName: "Ngày đăng",
       field: "createdAt",
-      valueGetter: ({ row }) => row.createdAt,
+      valueGetter: ({ row }) => format(new Date(row.createdAt), "dd/MM/yyyy HH:mm:ss"),
+      // flex: 1,
+      width: 200,
     },
     {
       headerName: "Lượt thích",
       field: "likes",
       valueGetter: ({ row }) => row.likes?.length,
+      flex: 1,
+      minWidth: 120,
+    },
+    {
+      headerName: "Lượt bình luận",
+      field: "comments",
+      valueGetter: ({ row }) => row.comments?.length,
+      flex: 1,
+      minWidth: 120,
     },
     {
       headerName: "Hành động",
@@ -61,7 +85,15 @@ const PostTable = () => {
       renderCell: (params) => (
         <Box>
           <Link to={""}>
-            <SoftButton variant="contained">button</SoftButton>
+            <Button
+              variant="outlined"
+              onClick={() => {
+                setPost(params.row);
+              }}
+            >
+              <VisibilityRounded color="info" size="large" />
+            </Button>
+            <DeleteAction params={params} />
           </Link>
           {/* <Link to={{ pathname: "/manage-course/edit/" + params.row._id }}>
             <SoftButton style={{ marginLeft: 16 }} variant="contained" startIcon={<Edit />} />
@@ -73,14 +105,9 @@ const PostTable = () => {
   ];
   const getData = async () => {
     const { data, total } = await getAllFeeds();
-    // console.log(data);
+    console.log(data);
     return { data, totalRows: total };
   };
-
-  // useEffect(() => {
-  //   getData();
-  // }, []);
-  // const getData = async () => ({ data: [], totalRow: 0 });
 
   return (
     <Stack gap={1} width="100%" height={500}>
@@ -103,5 +130,6 @@ PostTable.defaultProps = {
 PostTable.propTypes = {
   row: PropTypes.any,
   noGutter: PropTypes.bool,
+  setPost: PropTypes.any,
 };
 export default PostTable;
