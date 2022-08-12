fbe2397bae569bed140cffbc76a317e2d4b73444
hanguyenhaidang <dota.angiang76k@gmail.com>
2022-08-12 22:43:06 +0700

    host

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://freecourseapi.herokuapp.com/";
 export const HOME_SIDEBAR_WIDTH = 300;
 export const HOME_SIDEBAR_CLOSE_WIDTH = 90;
 export const NAVBAR_HEIGHT = 65;
